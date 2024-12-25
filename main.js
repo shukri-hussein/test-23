32f988debfdec4a30cf1e53906d7ae68dd984e87
shaku-hussien <nadahussein686@gmail.com>
2024-12-25 11:21:58 +0300

    day 23

---
+++ main.js
@@ -0,0 +1,8 @@
+                                    //Arrays
+
+let fuits = [1, "mango", 2, "orange", 3,"apple"];   
+console.log(fuits[3]);   
+
+for(let i = 0; fuits.length; i++) {
+    console.log(fuits);
+}
